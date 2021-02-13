353fa152487273322e164c6dcd0a69ea4bf90026
Hannah Malm <hannahemalm616@gmail.com>
2021-02-13 01:19:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -17,7 +17,7 @@ class EmailAddressParser
 
     #Begin parsing using a CLASS method - the CLASS is responsible for parsing, not each individual instance email 
     def parse
-      @emailparse
+      @emailparse.split()
 
     end 
 end 
